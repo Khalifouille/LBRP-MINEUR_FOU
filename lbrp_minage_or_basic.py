afac588c9f3b7b5b8c2172838ba289e398a21a0e
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2024-07-10 19:39:38 +0200

    Update lbrp_minage_or_basic.py

--- lbrp_minage_or_basic.py
+++ lbrp_minage_or_basic.py
@@ -1,57 +1,194 @@
-import win32gui
-import win32con
-import pyautogui
-import time
-import threading
+import numpy as np
+import win32gui, win32ui, win32con
+from PIL import Image
+from time import sleep, time
+import cv2 as cv
 import os
+from pynput.mouse import Button, Controller
+import random
+
+
+class WindowCapture:
+    w = 0
+    h = 0
+    hwnd = None
+
+    def __init__(self, window_name):
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception('Window not found: {}'.format(window_name))
+
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+
+        border_pixels = 8
+        titlebar_pixels = 30
+        self.w = self.w - (border_pixels * 2)
+        self.h = self.h - titlebar_pixels - border_pixels
+        self.cropped_x = border_pixels
+        self.cropped_y = titlebar_pixels
+
+    def get_screenshot(self):
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        img = img[..., :3]
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    def generate_image_dataset(self):
+        if not os.path.exists("images"):
+            os.mkdir("images")
+        while (True):
+            img = self.get_screenshot()
+            im = Image.fromarray(img[..., [2, 1, 0]])
+            im.save(f"./images/img_{len(os.listdir('images'))}.jpeg")
+            sleep(1)
+
+    def get_window_size(self):
+        return (self.w, self.h)
+
+
+class ImageProcessor:
+    W = 0
+    H = 0
+    net = None
+    ln = None
+    classes = {}
+    colors = []
+
+    def __init__(self, img_size, cfg_file, weights_file):
+        np.random.seed(42)
+        self.net = cv.dnn.readNetFromDarknet(cfg_file, weights_file)
+        self.net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+        self.ln = self.net.getLayerNames()
+        self.ln = [self.ln[i - 1] for i in self.net.getUnconnectedOutLayers()]
+        self.W = img_size[0]
+        self.H = img_size[1]
+
+        with open('yolov4-tiny/obj.names', 'r') as file:
+            lines = file.readlines()
+        for i, line in enumerate(lines):
+            self.classes[i] = line.strip()
+
+        self.colors = [
+            (0, 255, 0),
+            (0, 0, 255),
+            (255, 0, 0),
+            (255, 255, 0),
+            (255, 0, 255),
+            (0, 255, 255)
+        ]
+
+    def proccess_image(self, img):
+
+        blob = cv.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        outputs = self.net.forward(self.ln)
+        outputs = np.vstack(outputs)
+
+        coordinates = self.get_coordinates(outputs, 0.5)
+
+        self.draw_identified_objects(img, coordinates)
+
+        return coordinates
+
+    def get_coordinates(self, outputs, conf):
+
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        for output in outputs:
+            scores = output[5:]
+
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > conf:
+                x, y, w, h = output[:4] * np.array([self.W, self.H, self.W, self.H])
+                p0 = int(x - w // 2), int(y - h // 2)
+                boxes.append([*p0, int(w), int(h)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+        indices = cv.dnn.NMSBoxes(boxes, confidences, conf, conf - 0.1)
+
+        if len(indices) == 0:
+            return []
+
+        coordinates = []
+        for i in indices.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            coordinates.append(
+                {'x': x, 'y': y, 'w': w, 'h': h, 'class': classIDs[i], 'class_name': self.classes[classIDs[i]]})
+        return coordinates
+
+    def draw_identified_objects(self, img, coordinates):
+        for coordinate in coordinates:
+            x = coordinate['x']
+            y = coordinate['y']
+            w = coordinate['w']
+            h = coordinate['h']
+            classID = coordinate['class']
+
+            color = self.colors[classID]
+
+            cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv.putText(img, self.classes[classID], (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv.imshow('DETECTED OBJECTS', img)
+
+
+mouse = Controller()
+
+sleep(4)
 
 window_name = "Garry's Mod (x64)"
-images_to_search = [r"C:\Users\PC GAMER\Desktop\gmod_lbrp\seau_plein.png"]
-imageOffset = 25
+cfg_file_name = "./yolov4-tiny/yolov4-tiny-custom.cfg"
+weights_file_name = "yolov4-tiny-custom_last.weights"
 
-def find_and_bring_to_front(window_name):
-    hwnd = win32gui.FindWindow(None, window_name)
+wincap = WindowCapture(window_name)
+improc = ImageProcessor(wincap.get_window_size(), cfg_file_name, weights_file_name)
 
-    if hwnd:
-        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
-        win32gui.SetForegroundWindow(hwnd)
-        print(f"La fenêtre '{window_name}' a été mise en avant.")
-        return True
-    else:
-        print(f"La fenêtre '{window_name}' n'a pas été trouvée.")
-        return False
+while True:
 
-def check_images():
-    while True:
-        for image_path in images_to_search:
-            try:
-                pos = pyautogui.locateOnScreen(image_path, confidence=0.7)
-                if pos:
-                    pyautogui.moveTo(pos[0] + imageOffset, pos[1] + imageOffset)
-                    pyautogui.click()
-                    print(f"JE CLIQUE SUR : {image_path} !")
-                    os._exit(0)
-            except:
-                print(f"{image_path} n'est pas là !")
-                time.sleep(1)
+    ss = wincap.get_screenshot()
 
-def simulate_left_clicks(duration):
-    end_time = time.time() + duration
-    while time.time() < end_time:
-        pyautogui.click()
-        time.sleep(0.1)
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
 
-def main():
-    image_thread = threading.Thread(target=check_images)
-    image_thread.daemon = True
-    image_thread.start()
+    coordinates = improc.proccess_image(ss)
 
-    while True:
-        if find_and_bring_to_front(window_name):
-            time.sleep(5)
-            simulate_left_clicks(3 * 60)
-        else:
-            time.sleep(5)
+    coordinates = [c for c in coordinates if c["class_name"] == "Seau vide"]
 
-if __name__ == "__main__":
-    main()
+    if len(coordinates) == 0:
+        continue
+
+    seau_a_clique = coordinates[0]
+
+    mouse.position = (seau_a_clique['x'], seau_a_clique['y'])
+    mouse.press(Button.left)
+    sleep(0.05)
+    mouse.move(seau_a_clique['w'], seau_a_clique['h'])
+    sleep(0.05)
+    mouse.release(Button.left)
+
+print('Finished.')
